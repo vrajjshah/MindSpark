101c25c3972d8ff4075843a26612f1edfafe8321
Vraj Shah <vrajshah57@yahoo.in>
2018-03-11 12:25:19 +0530

    Login page

---
+++ footer.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html>
+<style type="text/css">
+	.footer{
+		width: 100%;
+		height:15%;
+		position: absolute;
+		bottom: 0px;
+		left: 0px;
+		background-color: rgb(168,189,191);
+	}
+		.text1{
+
+			color: white;
+			font-size: 80%;
+			font-family: verdana;
+			
+			
+
+
+		}
+
+	}
+</style>
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<body>
+	<div class="footer">
+		
+		<p class="text1">
+			Contact Co-ordinator
+			<br>
+			Vraj Patel
+			<br>
+			Mobile: <a href="tel:0937513154">09375131548</a>
+			<br>
+			Email: <a href="email:vraj.vup@gmail.com">vraj.vup@gmail.com</a>
+		</p>
+	
+		
+	</div>
+
+</body>
+</html>
---
+++ header.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html>
+<style type="text/css">
+	.header{
+		width: 100%;
+		height:20%;
+		position: absolute;
+		top: 0px;
+		left: 0px;
+		background-color: rgb(56,127,117);
+	}
+		.text{
+
+			color: white;
+			font-size: 200%;
+			font-family: verdana;
+			text-align: center;
+
+
+		}
+
+	}
+</style>
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<body>
+	<div class="header">
+		<p class="text">
+			MindSpark
+		</p>
+		
+	</div>
+
+</body>
+</html>
