37e1842e69d5600a51b044643c95d9b43ab213ff
vrajpatel2312 <vraj.vup@gmail.com>
2018-03-14 23:42:17 +0530

    ui

--- leaderboard.php
+++ leaderboard.php
@@ -2,14 +2,19 @@
 include 'includes/db_connect.php';
  ?>
 <!DOCTYPE html>
+<meta http-equiv="Refresh" content="#">
 <html>
 <head>
 	<title>Leaderboard</title>
+        
 </head>
 <body>
+        <?php 
+                include("header.php")
+         ?>
 <?php  
 				
-			$sql = 'SELECT `Teamname`,`levels`,`date` FROM `members` ORDER BY `levels` DESC, `date` DESC';
+			$sql = 'SELECT `Teamname`,`levels`,`date` FROM `members` ORDER BY `levels` DESC, `date` ASC';
 			mysqli_select_db($mysqli,'secure_login');
 			$retval = mysqli_query($mysqli,$sql);
 
@@ -21,11 +26,31 @@ include 'includes/db_connect.php';
 			}
 
                 ?>
-                <table border="1">
-                <?php while($row =mysqli_fetch_array($retval , MYSQLI_BOTH))
+                <div style="padding-top: 5%;">
+                <table class=" table " border="1" style="width: 50%;" align="center">
+                       
+                        <tr >
+                                <td class="table-dark">
+                                        Rank
+                                </td>
+                                <td class="table-dark">
+                                        Teamname
+                                </td>
+                                <td class="table-dark">
+                                        Level
+                                </td>
+                                <td class="table-dark">
+                                        Timestamp
+                                </td>
+                        </tr>
+                <?php $i=1;
+                while($row =mysqli_fetch_array($retval , MYSQLI_BOTH))
                 {
                 	?>
                 	<tr>
+                                <td>
+                                        <?php echo $i; ?>
+                                </td>
                 		<td>
                 			<?php echo "" .$row["Teamname"]; ?>
                 		</td>
@@ -38,7 +63,9 @@ include 'includes/db_connect.php';
                 		
                 		
                 	</tr>
-                	<?php } ?>
+
+                	<?php $i++;} ?>
                 </table>
+                </div>
 </body>
 </html>
