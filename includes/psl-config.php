4440aa8cdf32cb6cb1a35e1c2c249d3f0218b258
Vraj Shah <vrajshah57@yahoo.in>
2018-03-14 23:38:48 +0530

    trial for azure.
    older details in upper comment

--- includes/psl-config.php
+++ includes/psl-config.php
@@ -1,10 +1,19 @@
 <?php
 /**
  * These are the database login details
- */  
-define("HOST", "localhost");     // The host you want to connect to.
+ define("HOST", "localhost");     // The host you want to connect to.
 define("USER", "sec_user");    // The database username. 
 define("PASSWORD", "12345");    // The database password. 
+define("DATABASE", "secure_login");    // The database name.
+ 
+define("CAN_REGISTER", "any");
+define("DEFAULT_ROLE", "member");
+ 
+define("SECURE", FALSE);    // FOR DEVELOPMENT ONLY!!!!
+ */  
+define("HOST", "mindspark18.mysql.database.azure.com");     // The host you want to connect to.
+define("USER", "vraj@mindspark18");    // The database username. 
+define("PASSWORD", "asdASD123");    // The database password. 
 define("DATABASE", "secure_login");    // The database name.
  
 define("CAN_REGISTER", "any");
