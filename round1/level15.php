ac19310a1cf072a50e2b347bbd00fda2fd8ddcc9
vrajpatel2312 <vraj.vup@gmail.com>
2018-03-26 14:03:57 +0530

    15

--- round1/level15.php
+++ round1/level15.php
@@ -108,10 +108,7 @@ include_once 'includes/psl-config.php';
                         <div class="mask waves-effect waves-light"></div>
                     </a>
                 </div>
-           
-                <!--/Card image-->
-
-                <!--Card content-->
+                 <!--Card content-->
                 <div class="card-body text-center wow fadeIn" data-wow-delay="0.2s" style="visibility: visible; animation-name: fadeIn; animation-delay: 0.2s;">
                     <!--Title-->
                          <h4 class="card-title">SOLVE THE CLUE</h4>
@@ -127,13 +124,21 @@ include_once 'includes/psl-config.php';
                          <button class="btn btn-primary btn-lg waves-effect waves-light">SUBMIT</button>
                          </form>
                          <?php echo '<p class="blue-text">No. of Hints left : ' . $hint . ' </p>';
- 				       ?>
+                       ?>
                          <form action="level15.php" method="post">
-   	   				     <button name="hint" class="btn btn-default btn-lg waves-effect waves-light" value="GO"> Show Hint</button>
-				        </form>
-				
+                         <button name="hint" class="btn btn-default btn-lg waves-effect waves-light" value="GO"> Show Hint</button>
+                        </form>
+                
                 </div>
                 <!--/.Card content-->
+                <!--/Card image-->
+                    <div class="view overlay rgba-white-slight wow fadeIn" style="visibility: visible; animation-name: fadeIn;">
+                    <img src="level15.jpg" class="img-fluid">
+                    
+                        <div class="mask waves-effect waves-light"></div>
+                    </a>
+                </div>
+               
 
             </div>
             <!--/.Card-->
