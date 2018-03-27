22e4deefb154483bd295855f993a7bd3fc641d5f
vrajpatel2312 <vraj.vup@gmail.com>
2018-03-27 14:02:47 +0530

    d

--- round2/level6.php
+++ round2/level6.php
@@ -115,9 +115,7 @@ include_once 'includes/psl-config.php';
                 <div class="card-body text-center wow fadeIn" data-wow-delay="0.2s" style="visibility: visible; animation-name: fadeIn; animation-delay: 0.2s;">
                     <!--Title-->
                          <h4 class="card-title">SOLVE THE CLUE</h4>
-                         <p class="card-text" > <h3><embed src=" <?php
-  echo "file1.mp3";}
-?>" autostart="false" loop="false" />
+                         <p class="card-text" > <embed src="file1.mp3" autostart="false" loop="false" /></p>
 </h3>  </p>
  
                     
