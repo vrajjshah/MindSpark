1aadd2ddae434a1932daaccbdb68fb459bb46958
vrajpatel2312 <vraj.vup@gmail.com>
2018-03-27 16:25:02 +0530

    update

--- hint.php
+++ hint.php
@@ -27,7 +27,7 @@ include("head.php")
         <div class="card card-cascade wider reverse my-4 pb-5">
             <!--Card image-->
             <div class="view overlay rgba-white-slight wow fadeIn" style="visibility: visible; animation-name: fadeIn;">
-                <h4 style="padding-top: 2%; color: #E3468C;"><strong>Round 1 winners</strong></h4>
+                <h4 style="padding-top: 2%; color: #E3468C;"><strong>Hint</strong></h4>
                 
                 
                 <?php
